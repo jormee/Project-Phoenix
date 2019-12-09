10999d23d8337331d95e5a4c929cd322ec1ca46b
Chinedu Okorie <chinedu.okorie@pmglobaltechnology.com>
2019-12-09 19:13:35 +0100

    set up route

--- app.js
+++ app.js
@@ -9,7 +9,22 @@ app.use(cors());
 const port = process.env.PORT || 3002;
 
 
+app.get('/',(req,res)=>{
+    res.status(200).send("")
+})
+app.post("/dashboard",(req,res)=>{
 
+})
+
+app.post("/create-content",(req,res)=>{
+
+})
+app.post("/signup",(req,res)=>{
+
+})
+app.post("login",(req,res)=>{
+
+})
 
 app.listen(port,()=>{
     console.log("App started on "+port)
