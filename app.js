318291c97ea999a85a5d7c7427946ce122a9cf97
Chinedu Okorie <chinedu.okorie@pmglobaltechnology.com>
2019-12-10 17:44:59 +0100

    handle /create-content route

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const uuid = require("uuid");
 
 const app = express();
 
@@ -29,28 +30,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 //student schema
-const studentSchema = new mongoose.Schema({
-  studentfullname: {
+const noteSchema = new mongoose.Schema({
+  teacherUsername: {
     type: String
   },
-  studentClass: {
+  note: {
     type: String
   },
-  username: {
+
+  noteId: {
     type: String,
     required: true,
-    unique: true
+    unique:true
   },
-
-  pix: {
-    type: String
-  },
-
-  password: {
-    type: String,
-    required: true
-  },
-  date_registered: {
+  date_created: {
     type: Date,
     default: new Date()
   }
@@ -80,7 +73,7 @@ const teacherSchema = new mongoose.Schema({
   }
 });
 
-const Student = new mongoose.model("Student", studentSchema);
+const Note = new mongoose.model("Note", noteSchema);
 const Teacher = new mongoose.model("Teacher", teacherSchema);
 
 const port = process.env.PORT || 3003;
@@ -90,7 +83,44 @@ app.get("/", (req, res) => {
 });
 app.post("/dashboard", (req, res) => {});
 
-app.post("/create-content", (req, res) => {});
+app.get('/create-content',(req,res)=>{
+  let content = req.body.content;
+  axios.get(`https://www.googleapis.com/books/v1/volumes?q=${content}&key=${process.env.GOOGLE_BOOK_KEY}`,{
+    method:"GET",
+    headers:{
+      "content-type":"application/json"
+    }
+  })
+  .then(res2=>{
+    return res.status(200).json({
+      data:res2
+    })
+  })
+  .catch(err=>{console.log(err)})
+})
+
+app.post("/create-content", (req, res) => {
+  const {note,teacherUsername,} =req.body,
+  const newNote = new Note({
+    note:note,
+    teacherUsername:teacherUsername,
+    noteId: uuid(),
+  })
+  newNote.save((err)=>{
+    if(err){
+     return res.status(404).json({
+        status:"failed",
+        message:"could not save note"
+      })
+    }else{
+      return res.status(201).json({
+        status:"success",
+        message:"Note created"
+      })
+    }
+  })
+});
+
 app.post("/signup", (req, res) => {
     const{fullname,username,subject}= req.body;
     bcrypt.hash(req.body.password,10,(err,hash)=>{
@@ -99,14 +129,13 @@ app.post("/signup", (req, res) => {
           err: err
         })
       }else{
-        const teacher = new Teacher({
+        const newTeacher = new Teacher({
           fullname:fullname,
           userrname:username,
           subject:subject,
           password:hash
       })
-
-      teacher.save((err)=>{
+      newTeacher.save((err)=>{
         if(err){
           return res.status(400).json({
             status:"failed",
@@ -126,10 +155,11 @@ app.post("/signup", (req, res) => {
       }
     })
 });
+
+//login route
 app.post("/login", (req, res) => {
   const {username,password} = req.body
-
-  teacher.find({username:username},(err,foundteacher)=>{
+  Teacher.findOne({username:username},(err,foundteacher)=>{
     if(err){
       return res.status(404).json({
         status:"failed",
@@ -138,18 +168,21 @@ app.post("/login", (req, res) => {
       })
     }else{
       if(foundteacher){
-        if(foundteacher.password===password){
-          return res.status(200).json({
-            fullname:foundteacher.fullname,
-            subject:foundteacher.subject,
-            username:foundteacher.username
-          })
-        }else{
-          return res.status(401).json({
-            status:"failed",
-            message:"incorrect password"
-          })
-        }
+     bcrypt.compare(password, foundteacher.password, function(err, res2) {
+       if(res2===true){
+        return res.status(200).json({
+          fullname:foundteacher.fullname,
+          subject:foundteacher.subject,
+          username:foundteacher.username
+        })
+
+       }else{
+        return res.status(401).json({
+          status:"failed",
+          message:"incorrect password"
+        })
+       }
+       });
       }
     }
   })
